bfcedbdc4cbe4ebba8a9b1d6a273782bcedf8566
zhjrcc <zjrhello@gmail.com>
2024-04-29 21:28:44 +0800

    POST Request

--- express_tutorial/src/index.mjs
+++ express_tutorial/src/index.mjs
@@ -1,12 +1,13 @@
 import express from "express"
 import dotenv from "dotenv"
-
 dotenv.config()
-const app = express()
 
+const app = express()
 const PORT = process.env.PORT || 3001
 
-const mockUser = [
+app.use(express.json())
+
+const mockUsers = [
   { id: 1, username: "zhjrcc", email: "zjrhello@gmail.com" },
   { id: 2, username: "jack", email: "jack@outlook.com" },
   { id: 3, username: "lily", email: "lily@qq.com" },
@@ -18,15 +19,23 @@ app.get("/", (req, res) => {
   })
 })
 
+app.post("/api/users", (req, res) => {
+  console.log(req.body)
+  const {body} = req
+  const newUser = {id: mockUsers[mockUsers.length - 1].id + 1, ...body}
+  mockUsers.push(newUser)
+  res.status(201).send(newUser)
+})
+
 app.get("/api/users", (req, res) => {
   // console.log(req.query)
   const {
     query: { filter, value },
   } = req
   if (!filter || !value) {
-    return res.status(200).send(mockUser)
+    return res.status(200).send(mockUsers)
   }
-  const filterUser = mockUser.filter((user) => user[filter].includes(value))
+  const filterUser = mockUsers.filter((user) => user[filter].includes(value))
   return res.status(200).send(filterUser)
 })
 
@@ -38,7 +47,7 @@ app.get("/api/users/:id", (req, res) => {
   // console.log(req.params)
   const parsedId = parseInt(req.params.id)
   if (isNaN(parsedId)) res.sendStatus(400)
-  const findUser = mockUser.find((user) => user.id === parsedId)
+  const findUser = mockUsers.find((user) => user.id === parsedId)
   if (!findUser) res.sendStatus(404)
   res.send(findUser)
 })
