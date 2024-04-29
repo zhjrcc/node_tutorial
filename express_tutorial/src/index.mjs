91fe24f9bdc2718c2b2b69e9dc29c16d94ea86ba
zhjrcc <zjrhello@gmail.com>
2024-04-29 23:31:15 +0800

    Patch Request

--- express_tutorial/src/index.mjs
+++ express_tutorial/src/index.mjs
@@ -61,7 +61,20 @@ app.put("/api/users/:id", (req, res) => {
   if (isNaN(parsedId)) return res.sendStatus(400)
   const findUserIndex = mockUsers.findIndex((user) => user.id === parsedId)
   if (findUserIndex === -1) return res.sendStatus(404)
-  mockUsers[findUserIndex] = {id: parsedId, ...body}
+  mockUsers[findUserIndex] = { id: parsedId, ...body }
+  return res.sendStatus(200)
+})
+
+app.patch("/api/users/:id", (req, res) => {
+  const {
+    body,
+    params: { id },
+  } = req
+  const parsedId = parseInt(id)
+  if (isNaN(parsedId)) return res.sendStatus(400)
+  const findUserIndex = mockUsers.findIndex((user) => user.id === parsedId)
+  if (findUserIndex === -1) return res.sendStatus(404)
+  mockUsers[findUserIndex] = { ...mockUsers[findUserIndex], ...body }
   return res.sendStatus(200)
 })
 
