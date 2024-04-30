752ca9a29a7fa64fc0cb0b47add7f4a5262a997d
zhjrcc <zjrhello@gmail.com>
2024-05-01 03:06:58 +0800

    Validation-1

--- express_tutorial/src/index.mjs
+++ express_tutorial/src/index.mjs
@@ -1,7 +1,8 @@
 import express, { request } from "express"
-import dotenv from "dotenv"
-dotenv.config()
+import { config } from "dotenv"
+import { query, validationResult, body, matchedData } from "express-validator"
 
+config()
 const app = express()
 const PORT = process.env.PORT || 3001
 
@@ -31,31 +32,61 @@ const mockUsers = [
   { id: 3, username: "lily", email: "lily@qq.com" },
 ]
 
-app.get("/", (req, res) => {
+app.get("/", loggingMiddleware, (req, res) => {
   res.status(200).send({
     msg: "Hello Express",
   })
 })
 
-app.post("/api/users", (req, res) => {
-  console.log(req.body)
-  const { body } = req
-  const newUser = { id: mockUsers[mockUsers.length - 1].id + 1, ...body }
-  mockUsers.push(newUser)
-  res.status(201).send(newUser)
-})
+app.post(
+  "/api/users",
+  [
+    body("username")
+      .notEmpty()
+      .withMessage("不能为空")
+      .isLength({ min: 10, max: 100 })
+      .withMessage("用户名长度在5-10个字符"),
+    body("email").notEmpty().withMessage("邮箱不能为空"),
+  ],
+  (req, res) => {
+    const valiadtedResult = validationResult(req)
+    console.log(valiadtedResult)
 
-app.get("/api/users", loggingMiddleware, (req, res) => {
-  // console.log(req.query)
-  const {
-    query: { filter, value },
-  } = req
-  if (!filter || !value) {
-    return res.status(200).send(mockUsers)
+    if (!valiadtedResult.isEmpty())
+      return res.status(400).send({ error: valiadtedResult.array() })
+    const { body } = req
+    const newUser = { id: mockUsers[mockUsers.length - 1].id + 1, ...body }
+    mockUsers.push(newUser)
+    res.status(201).send(newUser)
   }
-  const filterUser = mockUsers.filter((user) => user[filter].includes(value))
-  return res.status(200).send(filterUser)
-})
+)
+
+app.get(
+  "/api/users",
+  query("filter")
+    .isString()
+    .withMessage("必须是字符串")
+    .notEmpty()
+    .withMessage("不能为空")
+    .isLength({ min: 5, max: 10 })
+    .withMessage("字符串长度范围在5-10个字符之间"),
+  (req, res) => {
+    // console.log(req.query)
+    const {
+      query: { value },
+    } = req
+
+    const valiadtedResult = validationResult(req)
+    console.log(valiadtedResult)
+    if (!valiadtedResult.isEmpty()) return res.status(400).send(mockUsers)
+    const data = matchedData(req)
+    const { filter } = data
+    const filterUser = mockUsers.filter((user) => {
+      if (user[filter]) return user[filter].includes(value)
+    })
+    return res.status(200).send(filterUser)
+  }
+)
 
 app.get("/api/products", (req, res) => {
   res.status(200).send([{ id: 1, name: "广州白切鸡", price: "99元" }])
@@ -63,7 +94,7 @@ app.get("/api/products", (req, res) => {
 
 app.get("/api/users/:id", resolveIndexByUserId, (req, res) => {
   // console.log(req.params)
-  const {findUserIndex} = req;
+  const { findUserIndex } = req
   const findUser = mockUsers[findUserIndex]
   res.status(200).send(findUser)
 })
@@ -81,7 +112,7 @@ app.patch("/api/users/:id", resolveIndexByUserId, (req, res) => {
 })
 
 app.delete("/api/users/:id", resolveIndexByUserId, (req, res) => {
-  const {findUserIndex} = req
+  const { findUserIndex } = req
   mockUsers.splice(findUserIndex, 1)
   res.status(200).send(mockUsers)
 })
